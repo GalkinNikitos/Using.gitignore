415e4fb229a8679cb01c0e3efb0f4d2c7cf79557
Nikita <nikson6911@mail.ru>
2022-11-10 22:18:05 +0300

    init

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import com.sun.tools.javac.Main;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void additional(){
+        assertEquals(2, calculator.addition(1, 1));
+    }
+}
